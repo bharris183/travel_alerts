404cc97371880e2bf1ddf6cef77ebab55791aad0
bahab <bharris183@gmail.com>
2021-03-08 23:26:01 -0500

    loading db from rss

--- app.go
+++ app.go
@@ -6,8 +6,12 @@ import (
 	"log"
     "net/http"
     "encoding/json"
+    "strconv"
+    "strings"
+    "time"
 
 	"github.com/gorilla/mux"
+    "github.com/ungerik/go-rss"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -38,8 +42,11 @@ func (a *App) Run(addr string) {
 
 func (a *App) initializeRoutes() {
     a.Router.HandleFunc("/threat/{country}", a.getThreat).Methods("GET")
+    //a.Router.HandleFunc("/rss", a.getThreatsFromRss).Methods("GET")
+    a.Router.HandleFunc("/update", a.updateThreatsFromRss).Methods("GET")
 }
 
+// Get a single threat from a country code
 func (a *App) getThreat(w http.ResponseWriter, r *http.Request) {
     a.addThreat()
     vars := mux.Vars(r)
@@ -78,4 +85,86 @@ func (a *App) addThreat() {
 	const strSql = `insert into RSS_THREATS (COUNTRY_CODE, THREAT_LEVEL, TITLE, LINK, DESCRIPTION, PUB_DATE ) 
     values ('SN', 3, 'Senegal is safe, mostly', 'http://sn.com', 'Senegal is a lovely place. Just stick to your hotel.', '2021-05-11' )`
 	a.DB.Exec(strSql)
+}
+
+// Loading our table
+
+func (a *App) getThreatsFromRss() ( []threat ) {
+
+	rssUrl := "https://travel.state.gov/_res/rss/TAsTWs.xml"
+
+	// Similar to reading a file, only this is rss
+	resp, err := rss.Read(rssUrl, false) // In rss package true for reddit feeds
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Result is a Channel struct representing the rss data
+	channel, err := rss.Regular(resp)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var threats []threat
+	// Now let's make our Threats with only the data we need
+	for _, item := range channel.Item {
+		var threat threat
+		title := item.Title
+		link := item.Link
+		description := item.Description
+		var pubDate time.Time
+		pd, err := item.PubDate.ParseWithFormat("Mon, 02 Jan 2006")
+		if err != nil {
+			pubDate = time.Now()
+		} else {
+			pubDate = pd
+		}
+		countryCode := getCountryCode(item.Category)
+		threatLevel := getThreatLevel(item.Category)
+		
+		threat.Title = title
+		threat.Link = link
+		threat.Description = description
+		threat.PubDate = pubDate
+		threat.CountryCode = countryCode
+		if i, err := strconv.Atoi(threatLevel); err == nil {
+			threat.ThreatLevel = i
+		} else {
+			threat.ThreatLevel = 0
+		}
+		threats = append(threats, threat)
+	}
+	return threats
+}
+
+func getThreatLevel(category []string) string {
+	for _, s := range category {
+		if strings.HasPrefix(s, "Level") {
+			strParts := strings.Split(s, ":")
+			s1 := strParts[0]
+			strParts = strings.Split(s1, " ")
+			return strParts[1]
+		}
+	}
+	return ""
+}
+
+func getCountryCode(category []string) string {
+	for _, s := range category {
+		if len(s) == 2 {
+			return s
+		}
+	}
+	return ""
+}
+
+func (a *App) updateThreatsFromRss(w http.ResponseWriter, r *http.Request) {
+    t := threats{LastUpdated: time.Now()}
+    t.Threats = a.getThreatsFromRss()
+    rowsUpdated, err := t.loadThreatsInDatbase(a.DB)
+    if err != nil {
+        fmt.Printf("Error loading threats from rss: %s", err)
+    } else {
+        fmt.Printf("%s rows updated", strconv.Itoa(rowsUpdated))
+    }  
 }
--- model.go
+++ model.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
     "database/sql"
-    "errors"
+	"log"
+	"strings"
 	"time"
 )
 
@@ -20,19 +22,53 @@ type threat struct {
 	PubDate     time.Time `json:"pubdate"`
 }
 
-const sqlGetProduct = `select COUNTRY_CODE, THREAT_LEVEL, TITLE, 
+const sqlGetThreat = `select COUNTRY_CODE, THREAT_LEVEL, TITLE, 
 	LINK, DESCRIPTION, PUB_DATE from RSS_THREATS 
 	where COUNTRY_CODE = ?` 
 
+const sqlInsertThreat = `insert into RSS_THREATS 
+	(COUNTRY_CODE, THREAT_LEVEL, TITLE, 
+		LINK, DESCRIPTION, PUB_DATE) values 
+	(?, ?, ?, ?, ?, ?)`
+
 
 func (t *threat) getThreat(db *sql.DB) error {
-    return db.QueryRow(sqlGetProduct, t.CountryCode).Scan(
+    return db.QueryRow(sqlGetThreat, t.CountryCode).Scan(
 		&t.CountryCode, &t.ThreatLevel, &t.Title, &t.Link, &t.Description, &t.PubDate)
 }
 
 
-func (t *threats) loadThreats(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (t *threats) loadThreatsInDatbase(db *sql.DB) (rowsUpdated int, err error) {
+	// For now we will not do anything with t.LastUpdated
+	
+	query := "insert into RSS_THREATS (COUNTRY_CODE, THREAT_LEVEL, TITLE, LINK, DESCRIPTION, PUB_DATE) values "
+	var inserts []string
+	var params []interface{}
+	for _, st := range t.Threats {
+		inserts = append(inserts, "(?, ?, ?, ?, ?, ?)")
+		params = append(params, st.CountryCode, st.ThreatLevel, st.Title, st.Link, st.Description, st.PubDate)
+	}
+    queryVals := strings.Join(inserts, ",")
+    query = query + queryVals
+    log.Println("query is", query)
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+    stmt, err := db.PrepareContext(ctx, query)
+    if err != nil {
+        log.Printf("Error %s when preparing SQL statement", err)
+        return 0, err
+    }
+    defer stmt.Close()
+    res, err := stmt.ExecContext(ctx, params...)
+    if err != nil {
+        log.Printf("Error %s when inserting row into rss_threats table", err)
+        return 0, err
+    }
+    rows, err := res.RowsAffected()
+    if err != nil {
+        log.Printf("Error %s when finding rows affected", err)
+        return 0, err
+    }
+    log.Printf("%d rss_threats created simulatneously", rows)
+    return int(rows), nil
   }
-
-
